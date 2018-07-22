39c9aad529781e91858b204dcaadbfb85ee1ae51
Yu Chen <jsnt_cy@hotmail.com>
2018-07-22 22:49:34 +0800

    create product catalog service and web api

---
+++ ECommerce/ECommerce.API/Models/ApiProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace ECommerce.API.Models
+{
+    public class ApiProduct
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("price")]
+        public double Price { get; set; }
+
+        [JsonProperty("isAvailable")]
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ ECommerce/ECommerce.ProductCatalog/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+using ECommerce.ProductCatalog.Model;
+
+namespace ECommerce.ProductCatalog
+{
+    interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetAllProductsAsync();
+
+        Task AddProductAsync(Product product);
+    }
+}
